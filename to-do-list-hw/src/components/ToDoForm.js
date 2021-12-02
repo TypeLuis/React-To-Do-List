ad306e4a7cdcc6a9e365e71eae5c2dd91ca84609
typeluis <typeluis@gmail.com>
2021-12-02 13:55:31 -0500

    styling changes

--- to-do-list-hw/src/components/ToDoForm.js
+++ to-do-list-hw/src/components/ToDoForm.js
@@ -6,10 +6,17 @@ const ToDoForm = (props) =>{
             <h1>Create New To Do</h1>
 
             <form onSubmit={props.handleSubmit}>
-            <input type="text" className='formText' name='toDo' value={props.switchValue ? '' : props.toDoForm } placeholder="Enter Todo Here"
-            onChange={(e)=>{props.setTodo(e.target.value); props.setSwitchValue(false)}} />
 
-            <input type='submit' id='submit' value='submit' />
+            <label for="to-do-form">Todo : </label>
+                
+                <input type="text" id="to-do-form" className='formText' name='toDo' value={props.switchValue ? '' : props.toDoForm } placeholder="Enter Todo Here"
+                onChange={(e)=>{props.setTodo(e.target.value); props.setSwitchValue(false)}} />
+                
+                
+
+                <input type='submit' id='submit' value='submit' />
+
+                {/* <label for="submit"></label> */}
             </form>
 
         </div>
--- to-do-list-hw/src/components/ToDoList.js
+++ to-do-list-hw/src/components/ToDoList.js
@@ -16,18 +16,17 @@ const ToDoList = (props) =>{
                         <>
                             {/* checks if item object key is true */}
                             {item.completed ?
-                            
-                            
+
                                 <div key={i} style={{animation: 'fade-in 2s ease-in' }} className='complete-box' >
 
-                                <div className='description-div'>
-                                    <p className='description'>{item.description}</p>
+                                <div className={'description-div complete-desc'}>
+                                    <h2 className='description'>{item.description}</h2>
                                 </div>
 
-                                <p className='complete'>Mark incomplete</p>
-                                <p className="complete-mark" onClick={(e)=>{props.handleToggle(i)}}>☒</p>    
+                                <h3 className='complete'>Mark incomplete</h3>
+                                <h4 className="complete-mark" onClick={(e)=>{props.handleToggle(i)}}>☒</h4>    
                                 </div>
-                        
+
                             :
 
                                 null
@@ -44,25 +43,24 @@ const ToDoList = (props) =>{
                 <h1>Incompleted Tasks</h1>
 
                 {props.toDoList.map((item, i)=>{
-            
-                
+
                     return(
-            
+
                         <>
                             {/* checks if item object key is true */}
                             {item.completed ?
-                        
+
                                 null
-                        
+
                             :
                                 <div key={i} style={{animation: 'fade-in 2s ease-in' }} className='incomplete-box'>
 
-                                <div className= 'description-div'>
-                                    <p className='description'>{item.description}</p>
+                                <div className= {'description-div incomplete-desc'}>
+                                    <h2 className='description'>{item.description}</h2>
                                 </div>
 
-                                <p className='incomplete'>Mark Complete </p>
-                                <p className="incomplete-mark" onClick={()=>{props.handleToggle(i)}}>✅</p>    
+                                <h3 className='incomplete'>Mark Complete </h3>
+                                <h4 className="incomplete-mark" onClick={()=>{props.handleToggle(i)}}>✅</h4>    
                                 </div>
                             }
 
